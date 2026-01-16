93aca69fb8e19597a4f7375f70fe0840eb9da23f
Nikhil <nk02622284@gmail.com>
2026-01-16 13:05:00 +0530

    Click me

--- qr_generator.py
+++ qr_generator.py
@@ -1,15 +1,11 @@
 import qrcode
-from PIL import Image
+import os
 
-def generate_qr(data, size=300, fill_color="black", back_color="white"):
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_H,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(data)
-    qr.make(fit=True)
-    img = qr.make_image(fill_color=fill_color, back_color=back_color)
-    img = img.resize((size, size))
-    return img
+def generate_qr(data):
+    os.makedirs("output", exist_ok=True)
+
+    qr = qrcode.make(data)
+    path = "output/qr.png"
+    qr.save(path)
+
+    return path
